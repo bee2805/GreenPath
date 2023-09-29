3f921adeb3c93c502036aa21184206e5e56c6eea
200089_Bee! <200089@virtualwindow.co.za>
2023-09-29 15:46:18 +0200

    Home Page Complete

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,8 +3,17 @@ import React from "react";
 function LandingPage () {
 
     return(
-        <div className="container">
-            <p>This is the landing page</p>
+        <div className="landing-container">
+            <div className="landingPage-info">
+                <div>
+                    <h2>Calculate your</h2>
+                    <h1>Carbon Footprint</h1>
+                </div>
+                
+                <p>Welcome to GreenPath, where every daily choice counts. Understand your impact, make eco-friendly decisions, and embark on a greener journey today.</p>
+
+                <button>Get Calculating</button>
+            </div>
         </div>
     )
 }
