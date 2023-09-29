419e503cc5ee67c4212c6eba797940a9520a816e
200089_Bee! <200089@virtualwindow.co.za>
2023-09-29 13:25:24 +0200

    setup pages and routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
+// Pages
+import LandingPage from "./pages/LandingPage";
+import CarbonCalculatorPage from "./pages/CarbonCalculatorPage";
+import GetInvolvedPage from "./pages/GetInvolvedPage";
+// React Routing
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/CarbonCalculator" element={<CarbonCalculatorPage/>}/>
+        <Route path="/GetInvolved" element={<GetInvolvedPage/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LandingPage () {
+
+    return(
+        <div className="container">
+
+        </div>
+    )
+}
+
+export default LandingPage;
