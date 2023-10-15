cc752e1fa3452c68be42dff2e564f4c9f2f3f3c6
200089_Bee! <200089@virtualwindow.co.za>
2023-10-15 17:22:49 +0200

    Updated landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,13 +6,13 @@ function LandingPage () {
         <div className="landing-container">
             <div className="landingPage-info">
                 <div>
-                    <h2>Calculate your</h2>
-                    <h1>Carbon Footprint</h1>
+                    <h2>Welcome to</h2>
+                    <h1>Green Path!</h1>
                 </div>
                 
-                <p>Welcome to GreenPath, where every daily choice counts. Understand your impact, make eco-friendly decisions, and embark on a greener journey today.</p>
+                <p>Welcome to GreenPath, where every daily choice counts. Green Path helps you understand your impact, make eco-friendly decisions, and embark on a greener journey starting today.</p>
 
-                <button>Get Calculating</button>
+                <button>Get Started</button>
             </div>
         </div>
     )
